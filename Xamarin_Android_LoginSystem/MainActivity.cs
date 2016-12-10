443229862ae24d657b626fe3e6ddba46e7666522
jack torres <jacintotorres92@outlook.com>
2016-12-10 15:54:58 -0500

    Add SignUp Event

--- Xamarin_Android_LoginSystem/MainActivity.cs
+++ Xamarin_Android_LoginSystem/MainActivity.cs
@@ -1,14 +1,17 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Threading;
 
 namespace Xamarin_Android_LoginSystem
 {
     [Activity(Label = "Xamarin_Android_LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-
+        
         private Button mBtnSignUp;
+        private ProgressBar mProgressBar;
+
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,6 +23,7 @@ namespace Xamarin_Android_LoginSystem
             SetContentView (Resource.Layout.Main);
 
             mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            mProgressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
 
             mBtnSignUp.Click += (object sender, System.EventArgs e) =>
             {
@@ -27,8 +31,24 @@ namespace Xamarin_Android_LoginSystem
                 FragmentTransaction transaction = FragmentManager.BeginTransaction();
                 dialog_SignUp signUpDialog = new dialog_SignUp();
                 signUpDialog.Show(transaction, "dialog fragment");
+
+                signUpDialog.mOnSignUpComplete += SignUpDialog_mOnSignUpComplete;
             };
         }
+
+        private void SignUpDialog_mOnSignUpComplete(object sender, OnSignUpEventArgs e)
+        {
+            mProgressBar.Visibility = Android.Views.ViewStates.Visible;
+            Thread thread = new Thread(ActLikeARequest);
+            thread.Start();
+        }
+
+        private void ActLikeARequest()
+        {
+            Thread.Sleep(3000);
+            RunOnUiThread( () => mProgressBar.Visibility = Android.Views.ViewStates.Invisible );
+        }
+
     }
 }
 
